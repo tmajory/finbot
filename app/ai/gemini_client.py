d16efed3438bf994a47abc57c5b4cf79746c2a66
tmajory <thaismajory2014@gmail.com>
2026-03-03 14:58:25 -0300

    refatora integração com a API Gemini e ajusta tratamento de respostas no bot Telegram

--- app/ai/gemini_client.py
+++ app/ai/gemini_client.py
@@ -1,23 +1,31 @@
-import google.generativeai as genai
-import PIL.Image
 import json
+import PIL.Image
+from google import genai
+
 
 
 class FinanceAnalyst:
 
     def __init__(self, api_key):
-        genai.configure(api_key=api_key)
-        self.model = genai.GenerativeModel('gemini-3-flash-preview')
+        client = genai.Client(api_key=api_key)
+        self.client = client
+        
         
 
-    def analise(self, message) -> dict:
+    def analise(self, message):
         prompt = f""" Extraia as informações deste gasto: "{message}"
-        Responda APENAS com JSON no formato:
-        {{"Valor":0.00,"Categoria":"String","Descricao":"string"}}
+        Responda APENAS com String no formato:
+        {{"Valor":'float',"Categoria":"String","Descricao":"String"}}
         Categoria deve ser generalizada e sempre iniciar com a letra maiúscula, exemplo se gasto em farmácia ou hospital então categoria é Saúde
         """
 
-        response = self.model.generate_content(prompt)
+        response = self.client.models.generate_content(model='gemini-2.5-flash',contents=prompt)
+        if response.text == None:
+            return {
+                'Success': False,
+                'Message': 'Retorno da genai vazio',
+                'data': None
+            }
         return json.loads(response.text)
 
     def image_analise(self, image_path) -> dict:
@@ -28,5 +36,23 @@ class FinanceAnalyst:
         {{"Valor":0.00, "Categoria":"string", "Descricao":"string"}}
         """
 
-        response = self.model.generate_content([prompt,img])
+        response = self.client.models.generate_content(model='gemini-2.5-flash',contents=[prompt,img])
+        if response.text == None:
+            return {
+                'Success': False,
+                'Message': 'Retorno da genai vazio',
+                'data': None
+            }
         return json.loads(response.text)
+    
+
+
+# if __name__ == '__main__':
+#     ai = FinanceAnalyst(api_key)
+#     # response  = ai.client.models.list()
+#     # print(response.page)
+#     response = ai.analise('100 reais em lanchonete')
+#     print(response)
+#     # txt = '```json\n{"Valor":100.0,"Categoria":"Alimentação","Descricao":"Gasto em lanchonete"}\n```'
+#     # txt = re.split('json',txt)
+#     # print(txt)
