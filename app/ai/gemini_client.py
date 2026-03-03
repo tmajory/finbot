8800155db0bba6340273db732fe9607ce6483f1b
tmajory <thaismajory2014@gmail.com>
2026-03-03 00:35:51 -0300

    adiciona suporte para análise de imagens e ajusta formatação de resposta no bot Telegram Fix future: Identificar se envio foi mensagem ou imagem e processar corretamente.

--- app/ai/gemini_client.py
+++ app/ai/gemini_client.py
@@ -1,4 +1,5 @@
 import google.generativeai as genai
+import PIL.Image
 import json
 
 
@@ -7,13 +8,25 @@ class FinanceAnalyst:
     def __init__(self, api_key):
         genai.configure(api_key=api_key)
         self.model = genai.GenerativeModel('gemini-3-flash-preview')
+        
 
-    def analise(self, message):
+    def analise(self, message) -> dict:
         prompt = f""" Extraia as informações deste gasto: "{message}"
         Responda APENAS com JSON no formato:
-        {{"valor":0.00,"categoria":"string","descricao":"string"}}
+        {{"Valor":0.00,"Categoria":"String","Descricao":"string"}}
         Categoria deve ser generalizada e sempre iniciar com a letra maiúscula, exemplo se gasto em farmácia ou hospital então categoria é Saúde
         """
 
         response = self.model.generate_content(prompt)
+        return json.loads(response.text)
+
+    def image_analise(self, image_path) -> dict:
+        img = PIL.Image.open(image_path)
+
+        prompt = f"""Extraia as informações deste comprovante/recibo,
+        Retorne apenas no formato:
+        {{"Valor":0.00, "Categoria":"string", "Descricao":"string"}}
+        """
+
+        response = self.model.generate_content([prompt,img])
         return json.loads(response.text)
